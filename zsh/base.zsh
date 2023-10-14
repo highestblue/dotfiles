d97590854b788ae769ff6e8b9177836056fc9998
Grant Lee <highestblue@gmail.com>
2023-10-14 10:47:24 -0700

    update

--- zsh/base.zsh
+++ zsh/base.zsh
@@ -86,7 +86,7 @@ ZSH_AUTOSUGGEST_STRATEGY=(completion)
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git zsh-syntax-highlighting zsh-autosuggestions)
+plugins=(git zsh-syntax-highlighting zsh-autosuggestions asdf)
 
 source $ZSH/oh-my-zsh.sh
 
@@ -113,16 +113,18 @@ source $ZSH/oh-my-zsh.sh
 # For a full list of active aliases, run `alias`.
 alias zshconfig="code ~/.zshrc"
 alias sourcezsh="source ~/.zshrc"
+alias ws="cd ~/workspace/hiive"
 
-if [[ $(uname) = Darwin ]]; then
-  include "$DOTFILES/zsh/nvm.zsh"
-fi
+# if [[ $(uname) = Darwin ]]; then
+#   include "$DOTFILES/zsh/nvm.zsh"
+# fi
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
 export PATH="$PATH:$HOME/.local/bin"
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# Don't need nvm if using asdf
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
